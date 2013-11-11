9ad9557c96d8524648020b768e4400d48f2f87fd
Adam Horne <ahorne@wordplaysoftware.com>
2013-11-11 17:35:29 -0500

    Added username and change sign_in to use username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,42 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  username               :string(255)
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :recoverable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  validates :username, presence: true, uniqueness: true,
+    length: { minimum: 3, maximum: 12 }
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
